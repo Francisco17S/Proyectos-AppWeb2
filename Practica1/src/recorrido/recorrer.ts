cb8013317b42bbb34a3cd820cb04d0b310497eaa
DairaCedeno <106940394+DairaCedeno@users.noreply.github.com>
2024-04-25 23:49:57 -0500

    recorrer.ts

---
+++ Practica1/src/recorrido/recorrer.ts
@@ -0,0 +1,20 @@
+import { computadora, computadoras, prestamista, prestamistas, prestamo, prestamos } from "../arreglos/arreglos";
+
+
+
+export function mostrarDatos() {
+    console.log("Computadoras");
+    computadoras.forEach((computadora: computadora) => {
+        console.log(computadora);
+    });
+
+    console.log("Encuentros Deportivos:");
+    for (let prestamista of prestamistas) {
+        console.log(prestamista);
+    }
+
+    console.log("Pronósticos:");
+    for (let i in prestamos) {
+        console.log(prestamos[i]);
+    }
+}
